429c41e3d3919039f1932cf80a58b0ec91f9912d
gentasheesh <111467446+gentasheesh@users.noreply.github.com>
2022-08-18 12:14:32 +0800

    Update ip.py

--- ip.py
+++ ip.py
@@ -26,7 +26,7 @@ Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
                        ]
 
 
-print("Attack initiated no ip: %s e Porta: %s"%(orgip,port))
+print(" Attack by Genta Script.py ip: %s e Porta: %s"%(orgip,port))
 
             
 
